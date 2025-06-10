e283866ea957a769288eac24bc0e5b1a12894f6c
Omar Jarid <omarjarid1993@gmail.com>
2025-06-10 14:25:15 +0200

    Improve use of themes

--- app/src/main/java/com/omarjarid/noasanapp/MainScreen.kt
+++ app/src/main/java/com/omarjarid/noasanapp/MainScreen.kt
@@ -1,11 +1,13 @@
 package com.omarjarid.noasanapp
 
+import android.content.res.Resources.Theme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,7 +30,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         modifier = modifier.fillMaxSize()
     ) {
-        Text(reasonModel.reason, textAlign = TextAlign.Center)
+        Text(reasonModel.reason, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.primary)
         Spacer(modifier = Modifier.height(8.dp))
         Button(onClick = { reasonViewModel.getReason() }) {
             Text(stringResource(R.string.button_text))
