fdd47d0602f862a126a80d66f43d2afc811e6bd8
Omar Jarid <omarjarid1993@gmail.com>
2025-06-13 15:33:22 +0200

    Fix Splash Screen duration

--- app/src/main/java/com/omarjarid/noasanapp/MainActivity.kt
+++ app/src/main/java/com/omarjarid/noasanapp/MainActivity.kt
@@ -5,14 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.omarjarid.noasanapp.presentation.MainScreen
 import com.omarjarid.noasanapp.ui.theme.NoAsAnAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private var isLoading = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { isLoading }
+        lifecycleScope.launch {
+            delay(2000)
+            isLoading = false
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
